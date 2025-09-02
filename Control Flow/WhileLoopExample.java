51e9e98f92312ce8001d2de3170ef857cfbc3687
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-02 20:30:43 +0530

    Create WhileLoopExample.java

---
+++ Control Flow/WhileLoopExample.java
@@ -0,0 +1,11 @@
+// ControlFlow/WhileLoopExample.java
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Numbers from 1 to 5 using while loop:");
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
