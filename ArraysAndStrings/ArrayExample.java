2fc280df26fa46b654d1a2b5eaf72c86e3a5a59b
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-05 07:19:02 +0530

    Update ArrayExample.java

--- ArraysAndStrings/ArrayExample.java
+++ ArraysAndStrings/ArrayExample.java
@@ -1,29 +1,52 @@
-// ArraysAndStrings/ArrayExample.java
-public class ArrayExample {
-    public static void main(String[] args) {
-        // Declare and initialize an array
-        int[] numbers = {10, 20, 30, 40, 50};
+import java.util.Scanner;
 
-        // Print all elements using for loop
-        System.out.println("Elements of the array:");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println("Element at index " + i + ": " + numbers[i]);
+public class ArrayConceptsDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. Ask user for array size
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        // 2. Declare and create the array
+        int[] numbers = new int[size];
+
+        // 3. Input elements from the user
+        System.out.println("\nEnter " + size + " integer elements:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element at index " + i + ": ");
+            numbers[i] = scanner.nextInt();
         }
 
-        // Sum of array elements
+        // 4. Print all elements using for-each loop
+        System.out.println("\nYou entered:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        // 5. Calculate sum and average
         int sum = 0;
         for (int num : numbers) {
             sum += num;
         }
-        System.out.println("Sum of array elements: " + sum);
+        double average = (double) sum / size;
 
-        // Find maximum element
+        // 6. Find maximum and minimum elements
         int max = numbers[0];
+        int min = numbers[0];
         for (int num : numbers) {
-            if (num > max) {
-                max = num;
-            }
+            if (num > max) max = num;
+            if (num < min) min = num;
         }
+
+        // 7. Display results
+        System.out.println("\nArray Length: " + numbers.length);
+        System.out.println("Sum of elements: " + sum);
+        System.out.println("Average of elements: " + average);
         System.out.println("Maximum element: " + max);
+        System.out.println("Minimum element: " + min);
+
+        scanner.close();
     }
 }
