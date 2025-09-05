369c8d874ec5a0b96e21e287a31cac495e5f34d9
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-05 07:14:02 +0530

    Create Break.java

---
+++ Control Flow/Break.java
@@ -0,0 +1,11 @@
+public class BreakExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                break; // Exit the loop when i == 3
+            }
+            System.out.println("i = " + i);
+        }
+        System.out.println("Loop ended.");
+    }
+}
